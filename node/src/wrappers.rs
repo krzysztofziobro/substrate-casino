ec7beb794c5e9ca593a7929de5a548e4e6d9040a
Krzysztof Ziobro <krzysztof.ziobro@cardinals.cc>
2022-05-30 09:46:44 +0200

    Casino: first working version

--- node/src/wrappers.rs
+++ node/src/wrappers.rs
@@ -50,7 +50,7 @@ where
 {
 	type Error = ConsensusError;
 
-	async fn on_start(&mut self) -> Vec<(Block::Hash, NumberFor<Block>)> {
+ 	async fn on_start(&mut self) -> Vec<(Block::Hash, NumberFor<Block>)> {
         self.grandpa_block_import.on_start().await
 	}
 
@@ -80,7 +80,7 @@ where
 	type Error = ConsensusError;
 	type Transaction = TransactionFor<Client, Block>;
 
-	async fn import_block(
+ 	async fn import_block(
 		&mut self,
 		block: BlockImportParams<Block, Self::Transaction>,
 		new_cache: HashMap<well_known_cache_keys::Id, Vec<u8>>,
@@ -94,12 +94,16 @@ where
                 let mut avg_block_diff : u128 = 0;
                 for i in 0..self.block_timestamps.len()-1 {
                     avg_block_diff += self.block_timestamps[i+1] - self.block_timestamps[i];
-                }
+                 }
                 avg_block_diff /= (N - 1) as u128;
                 let estimate = self.block_timestamps[self.block_timestamps.len() - 1] + K as u128 * avg_block_diff;
 
                 info!("Casino Block Import estimate: {}", estimate);
                 self.sender.send(CasinoMessage(estimate, block.header.hash())).unwrap();
+             }
+
+            if block_num == N + K {
+                info!("Actual timestamp for block K + N: {}", timestamp);
             }
 
 	        self.grandpa_block_import.import_block(block, new_cache).await
@@ -150,10 +154,15 @@ impl<Block : BlockT> Validator<Block> for CasinoValidator {
         who: &PeerId,
         data: &[u8],
         ) -> sc_network_gossip::ValidationResult<Block::Hash> {
+
+            let mut mut_data = data.clone();
+            let message : CasinoMessage<Block> = CasinoMessage::decode(&mut mut_data).unwrap();
+
             info!("Validating gossip message: ");
             info!("Author: {}", who);
-            info!("Data: {:?}", data);
-            sc_network_gossip::ValidationResult::Discard
+            info!("Timestamp: {}", message.0);
+
+            sc_network_gossip::ValidationResult::ProcessAndKeep(message.1)
     }
 }
 
@@ -180,7 +189,9 @@ impl <B : BlockT> CasinoGossipEngine<B> {
 use std::task::{Poll, Context};
 use std::pin::Pin;
 use std::future::Future;
+use sp_api::Decode;
 
+#[derive(Clone, Copy, Encode, Decode)]
 pub struct CasinoMessage<B: BlockT>(u128, B::Hash);
 
 impl<B: BlockT> Future for CasinoGossipEngine<B> {
@@ -192,7 +203,7 @@ impl<B: BlockT> Future for CasinoGossipEngine<B> {
                 Ok(CasinoMessage(estimate, block_hash)) => {
                     info!("Casino Gossip Engine recieved estimate: {}", estimate);
 
-                    self.gossip_engine.gossip_message(block_hash, estimate.encode(), false);
+                    self.gossip_engine.gossip_message(block_hash, CasinoMessage::<B>(estimate, block_hash).encode(), false);
 
                     info!("Message gossiped");
                 },
